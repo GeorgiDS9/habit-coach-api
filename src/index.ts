ade9711e380b63067173e0f00a44f9770e21c288
georgiana <gchiriac2012@gmail.com>
2026-03-23 20:10:22 +0100

    feat: add Apollo ping bootstrap endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,32 @@
-console.log("habit-coach-api bootstrap");
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+const typeDefs = `#graphql
+  type Query {
+    ping: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    ping: () => "pong",
+  },
+};
+
+async function bootstrap() {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+  });
+
+  console.log(`GraphQL server ready at ${url}`);
+}
+
+bootstrap().catch((error) => {
+  console.error("Failed to start server:", error);
+  process.exit(1);
+});
